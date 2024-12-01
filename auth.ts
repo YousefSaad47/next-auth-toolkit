5c6af9d0e796de4ba62669a8b0599864a8b325b1
yousef <YoussefSaad299@gmail.com>
2024-12-01 15:17:13 +0200

    feat(auth): extend session and jwt callbacks to include user role and id

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from './lib/db';
 import authConfig from '@/auth.config';
+import { getUserById } from '@/data/user';
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +10,34 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async jwt({ token }) {
+      if (!token.sub) {
+        return token;
+      }
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) {
+        return token;
+      }
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
